01e68e94cd4f5c1413a586c4c5f0b472c0c23842
Pedro Ignacio Perez Vargas <pevargas@dc.uba.ar>
2025-03-20 02:04:09 -0300

    Add register aliases to the wavetrace

--- src/RegisterFile.sv
+++ src/RegisterFile.sv
@@ -43,4 +43,44 @@ module RegisterFile (
     end
   end
 `endif
+`ifdef PRETTY_WAVETRACE
+  // verilator lint_off UNUSEDSIGNAL
+  word zero, ra, sp, gp, tp;
+  word t0, t1, t2, t3, t4, t5, t6;
+  word s0, s1, s2, s3, s4, s5, s6, s7, s8, s9, s10, s11;
+  word a0, a1, a2, a3, a4, a5, a6, a7;
+  assign zero = regs[0];
+  assign ra   = regs[1];
+  assign sp   = regs[2];
+  assign gp   = regs[3];
+  assign tp   = regs[4];
+  assign t0   = regs[5];
+  assign t1   = regs[6];
+  assign t2   = regs[7];
+  assign s0   = regs[8];
+  assign s1   = regs[9];
+  assign a0   = regs[10];
+  assign a1   = regs[11];
+  assign a2   = regs[12];
+  assign a3   = regs[13];
+  assign a4   = regs[14];
+  assign a5   = regs[15];
+  assign a6   = regs[16];
+  assign a7   = regs[17];
+  assign s2   = regs[18];
+  assign s3   = regs[19];
+  assign s4   = regs[20];
+  assign s5   = regs[21];
+  assign s6   = regs[22];
+  assign s7   = regs[23];
+  assign s8   = regs[24];
+  assign s9   = regs[25];
+  assign s10  = regs[26];
+  assign s11  = regs[27];
+  assign t3   = regs[28];
+  assign t4   = regs[29];
+  assign t5   = regs[30];
+  assign t6   = regs[31];
+  // verilator lint_on UNUSEDSIGNAL
+`endif
 endmodule
