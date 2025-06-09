2212e34e5afc9239810922dc652447df509afc27
BlueTheDuck <hello@perezv.ar>
2025-06-08 22:38:07 -0300

    Format CpuWrapper

--- src/CpuWrapper.sv
+++ src/CpuWrapper.sv
@@ -26,21 +26,21 @@ module CpuWrapper (
   AvalonMmRw data_manager ();
   AvalonMmRead instruction_manager ();
 
-  assign avalon_data_manager_address        = data_manager.address;
-  assign avalon_data_manager_byteenable     = data_manager.byteenable;
-  assign data_manager.waitrequest           = avalon_data_manager_waitrequest;
-  assign avalon_data_manager_write          = data_manager.write;
-  assign avalon_data_manager_host_to_agent  = data_manager.host_to_agent;
-  assign avalon_data_manager_read           = data_manager.read;
-  assign data_manager.agent_to_host         = avalon_data_manager_agent_to_host;
-  assign data_manager.readdatavalid         = avalon_data_manager_readdatavalid;
+  assign avalon_data_manager_address           = data_manager.address;
+  assign avalon_data_manager_byteenable        = data_manager.byteenable;
+  assign data_manager.waitrequest              = avalon_data_manager_waitrequest;
+  assign avalon_data_manager_write             = data_manager.write;
+  assign avalon_data_manager_host_to_agent     = data_manager.host_to_agent;
+  assign avalon_data_manager_read              = data_manager.read;
+  assign data_manager.agent_to_host            = avalon_data_manager_agent_to_host;
+  assign data_manager.readdatavalid            = avalon_data_manager_readdatavalid;
 
-  assign avalon_instruction_manager_address = instruction_manager.address;
+  assign avalon_instruction_manager_address    = instruction_manager.address;
   assign avalon_instruction_manager_byteenable = instruction_manager.byteenable;
-  assign instruction_manager.waitrequest    = avalon_instruction_manager_waitrequest;
-  assign avalon_instruction_manager_read    = instruction_manager.read;
-  assign instruction_manager.agent_to_host  = avalon_instruction_manager_agent_to_host;
-  assign instruction_manager.readdatavalid  = avalon_instruction_manager_readdatavalid;
+  assign instruction_manager.waitrequest       = avalon_instruction_manager_waitrequest;
+  assign avalon_instruction_manager_read       = instruction_manager.read;
+  assign instruction_manager.agent_to_host     = avalon_instruction_manager_agent_to_host;
+  assign instruction_manager.readdatavalid     = avalon_instruction_manager_readdatavalid;
 
 
   Cpu cpu (
