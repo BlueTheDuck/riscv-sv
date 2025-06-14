5989eeed1c5dbf6ab77d8fa2c18a9e315f72587d
BlueTheDuck <hello@perezv.ar>
2025-06-14 03:43:11 -0300

    Rewrite the C runtime in assembly

--- sw/bsp.c
+++ sw/bsp.c
@@ -2,18 +2,9 @@
 
 #define loop for (;;)
 
-volatile uint32_t *DATA_OUT = (uint32_t *)0xF0000000;
-extern uint32_t _bss_start, _bss_end;
+volatile uint32_t *EXIT = (uint32_t *)0xF0000000;
 
-void _start() {
-  asm volatile("lui sp, %hi(__stack_top$)\n"
-               "addi sp, sp, %lo(__stack_top$)");
-  asm volatile("lui gp, %hi(__global_pointer$)\n"
-               "addi gp, gp, %lo(__global_pointer$)");
-  for (uint32_t *bss = &_bss_start; bss != &_bss_end; bss++) {
-    *bss = 0;
-  }
-  main();
-  *DATA_OUT = 0x0;
-  loop {}
+void exit(int32_t status) {
+  *EXIT = status;
+  loop;
 }
---
+++ sw/crt0.s
@@ -0,0 +1,32 @@
+.extern __stack_top$
+.extern __global_pointer$
+.extern main
+.extern exit
+
+.section .text._start
+.global _start
+
+_start:
+
+.option push
+.option norelax
+  la sp, __stack_top$
+  la gp, __global_pointer$
+.option pop
+
+.bss_init:
+  la a5, _bss_start
+  la a4, _bss_end
+1:
+  beq  a5, a4, 2f
+  sw   zero, 0(a5)
+  addi a5, a5, 4
+  j    1b
+2:
+  # main(0, NULL);
+  li   a0, 0
+  li   a1, 0
+  call main
+  tail exit
+halt:
+  j halt
