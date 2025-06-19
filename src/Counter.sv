2a201d0d5785f40fdf2328c75ef7868e61649f65
BlueTheDuck <hello@perezv.ar>
2025-06-19 00:44:02 -0300

    Rename module to "Counter"

--- src/Counter.sv
+++ src/Counter.sv
@@ -1,4 +1,10 @@
-module ProgramCounter (
+//! Counter module that increments or loads a value based on control signals
+//! Specification, in order of precedence:
+//! - ¬rst => out = 0
+//! - rst ∧ load => out = in
+//! - rst ∧ ¬load ∧ increment => out = out + step
+//! - rst ∧ ¬load ∧ ¬increment => out = out
+module Counter (
     input bit clk,
     input bit rst,
     input bit increment,
@@ -7,13 +13,11 @@ module ProgramCounter (
     input Types::int32_t in,
     output Types::int32_t out
 );
-  initial begin
-    out = 'x;
-  end
+  initial out = 'x;
 
   always_ff @(posedge clk or negedge rst) begin
     if (rst == 0) begin
-      out <= 32'b0;
+      out <= 0;
     end else begin
       if (load) begin
         out <= in;
