edf4f5825683d1e31d1070b37bb6d4c495e375b6
Pedro Ignacio Perez Vargas <pevargas@dc.uba.ar>
2025-08-02 13:58:35 -0300

    Improve gcc build script
    - There is no install target...
    - Fix compiler build script:
      - Don't build 64 bit support
      - Change ABI to softfloat
    - Cache git submodules
    - (optional) install linux user gcc

--- sw/build-toolchain.sh
+++ sw/build-toolchain.sh
@@ -4,7 +4,7 @@ sudo sh -c "mkdir -p /opt/riscv && chown $USER /opt/riscv"
 
 docker build -t rv32i:20250522 .
 CONTAINER_ID=$(docker run -d rv32i:20250522)
-docker cp ${CONTAINER_ID}:/opt/riscv /opt/riscv
+docker cp ${CONTAINER_ID}:/opt/riscv /opt/
 docker rm -f ${CONTAINER_ID}
 echo "Toolchain built and copied to /opt/riscv"
 echo "Run 'docker rmi rv32i:20250522' to remove the image if no longer needed."
