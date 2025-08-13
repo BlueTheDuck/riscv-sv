2af92efee9a3ca7f3b09da5c49c531f24272da3a
BlueTheDuck <hello@perezv.ar>
2025-08-12 22:05:58 -0300

    IMU format

--- src/InstructionManagerUnit.sv
+++ src/InstructionManagerUnit.sv
@@ -1,4 +1,6 @@
-module InstructionManagerUnit import Types::*; (
+module InstructionManagerUnit
+  import Types::*;
+(
     input logic clk,
     input logic rst,
 
@@ -8,43 +10,51 @@ module InstructionManagerUnit import Types::*; (
     input bit fetch_next_instruction,
 
     output bit ready,
-    output uint32_t ir,
+    output uint32_t ir
 );
-    enum int { IDLE, PUT_DATA_ON_BUS, WAITING_FOR_RESPONSE } state;
+  enum int {
+    IDLE,
+    PUT_DATA_ON_BUS,
+    WAITING_FOR_RESPONSE
+  } state;
 
-    assign instruction_manager.read = state == PUT_DATA_ON_BUS;
-    assign instruction_manager.byteenable = 4'b1111;
-    assign ready = state == IDLE;
+  assign instruction_manager.read = state == PUT_DATA_ON_BUS;
+  assign instruction_manager.byteenable = 4'b1111;
+  assign ready = state == IDLE;
 
-    always_ff @(posedge clk or negedge rst) begin
-        if (!rst) begin
-            state <= IDLE;
-        end else begin
-            unique case (state)
-                IDLE: if(fetch_next_instruction) begin
-                    instruction_manager.address <= pc;
-                    state <= PUT_DATA_ON_BUS;
-                end
-            
-                PUT_DATA_ON_BUS: if(instruction_manager.readdatavalid) begin
-                    ir <= instruction_manager.agent_to_host;
-                    state <= IDLE;
-                end else if(instruction_manager.waitrequest) begin
-                    state <= PUT_DATA_ON_BUS;
-                end else begin
-                    state <= WAITING_FOR_RESPONSE;
-                end
-                
-                WAITING_FOR_RESPONSE: if(instruction_manager.readdatavalid) begin
-                    ir <= instruction_manager.agent_to_host;
-                    state <= IDLE;
-                end
-            endcase
+  always_ff @(posedge clk or negedge rst) begin
+    if (!rst) begin
+      state <= IDLE;
+      ir <= 0;
+    end else begin
+      unique case (state)
+        IDLE:
+        if (fetch_next_instruction) begin
+          instruction_manager.address <= pc;
+          state <= PUT_DATA_ON_BUS;
         end
-    end
 
-    task automatic simulate_ready(input uint32_t in);
-        ir <= in;
-        state <= IDLE;
-    endtask
+        PUT_DATA_ON_BUS:
+        if (instruction_manager.readdatavalid) begin
+          ir <= instruction_manager.agent_to_host;
+          state <= IDLE;
+        end else if (instruction_manager.waitrequest) begin
+          state <= PUT_DATA_ON_BUS;
+        end else begin
+          state <= WAITING_FOR_RESPONSE;
+        end
+
+        WAITING_FOR_RESPONSE:
+        if (instruction_manager.readdatavalid) begin
+          ir <= instruction_manager.agent_to_host;
+          state <= IDLE;
+        end
+      endcase
+    end
+  end
+
+  task automatic simulate_ready(input uint32_t in);
+    ir <= in;
+    state <= IDLE;
+  endtask
 endmodule
